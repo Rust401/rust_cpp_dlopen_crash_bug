a13b44a4667b6bdfb95dcfc8dcde10cfea448ad0
rust <31315527+Rust401@users.noreply.github.com>
2024-12-28 14:27:42 +0800

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+// src/lib.rs                                
+                                             
+#[cxx::bridge]    
+mod ffi {
+    extern "Rust" {                          
+        fn rust_hello_world();               
+    }                                        
+}
+
+fn rust_hello_world() {                      
+    println!("Hello from Rust!");            
+}                                            
+
+#[no_mangle]                                 
+pub extern "C" fn rust_hello_world_extern() {
+    rust_hello_world();                      
+}
