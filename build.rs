609f6f0bfaad8a0c96a90f006156a73809b49570
rust <31315527+Rust401@users.noreply.github.com>
2024-12-28 14:26:27 +0800

    Create build.rs

---
+++ build.rs
@@ -0,0 +1,6 @@
+// build.rs
+fn main() {                                       
+    let _build = cxx_build::bridge("src/lib.rs"); 
+                                                  
+    println!("cargo:rerun-if-changed=src/lib.rs");
+}                                                 
